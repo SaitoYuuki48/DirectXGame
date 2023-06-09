ca407d3745296eca76afc0dab55fd8140e77926d
SaitoYuuki48 <k022g0048@g.neec.ac.jp>
2023-06-09 10:18:37 +0900

    途中

---
+++ Enemy.cpp
@@ -0,0 +1,27 @@
+﻿#include "Enemy.h"
+#include <assert.h>
+
+void Enemy::Initialize(Model* model, const Vector3& position, const Vector3& velocity) {
+	// 引数から受け取ったモデルが読み込まれているかチェック
+	assert(model);
+
+	 model_ = model;
+	// テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("debugfont.png");
+	
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	// 引数で受け取った初期座標をセット
+	worldTransform_.translation_ = position;
+
+
+}
+
+void Enemy::Update() {
+	// ワールド行列の更新
+	worldTransform_.UpdateMatrix();
+}
+
+void Enemy::Draw(const ViewProjection& viewProjection) {
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
---
+++ Enemy.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "Input.h"
+#include "Model.h"
+#include "WorldTransform.h"
+
+class Enemy {
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="model">モデル</param>
+	/// <param name="position">初期座標</param>
+	void Initialize(Model* model, const Vector3& position, const Vector3& velocity);
+
+	///< summary>
+	/// 更新
+	///  </summary>
+	void Update();
+
+	///< summary>
+	/// 描画
+	///  </summary>
+	/// <param name="viewPrejection& viewProjection
+	void Draw(const ViewProjection& viewProjection);
+
+private: // 変数
+	// ワールド変換データ
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+};
